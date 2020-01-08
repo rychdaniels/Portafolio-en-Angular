9d2b6842389ab9c5e68260788580270fc40a3917
rychdaniels <rychraya@gmail.com>
2020-01-08 14:50:03 -0600

    Quinta parte del curso terminada. Se empezo a utiliza firebase para alamcenar los datos de la pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,35 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
 
     // console.log('Servicio de infoPagina listo!');
+    this.cargarInfo();
+    this.cargarEquipo();
     
+
+  }
+
+  private cargarInfo(){
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(this.info);
-      });
+        
+    });
+  }
 
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-49e53.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.cargada = true;       
+        this.equipo = resp;
+        console.log(this.equipo);
+      });
   }
 }
