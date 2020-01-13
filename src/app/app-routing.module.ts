e77bf201b987f20be64903bf84c829483060c8c6
rychdaniels <rychraya@gmail.com>
2020-01-13 14:21:03 -0600

    Seccion 7 terminada. Cada producto tiene su propia vista, asi como su imagen y descripcion. Se implemento la posibilidad de realizar busquedas por medio de la categoria y el titulo del producto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component'
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component'
 
 //Constante donde se alamcenarán las rutas para la aplicacion
 const app_routes: Routes = [    
     // Si la dirrecion es vacio
     { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent }, //Si la direccion contiene 'about'
-    { path: 'item', component: ItemComponent },
+    { path: 'item/:id', component: ItemComponent },
+    { path: 'search/:termino', component: SearchComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'home' }//Si no es ninguna de las rutas anteriores, me redirecciona al home
 
 ];
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,6 +10,7 @@ export class ProductosService {
   // Bandera para saber cuando esta cargando los datos
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
 
   constructor( private http: HttpClient ) { 
@@ -18,16 +19,58 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-49e53.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
-        console.log(resp);
-        this.productos = resp;
+    // Se usa una promesa para asegurarnos que al realizar una busqueda los productos ya se encuentren cargados
+    return new Promise( (resolve, reject) => {
 
-        setTimeout(() => {
-          this.cargando = false;
-        }, 1200)
-        
+      this.http.get('https://angular-html-49e53.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
+          // console.log(resp);
+          this.productos = resp;
+          setTimeout(() => {
+            this.cargando = false;
+          }, 800);
+          resolve();          
+      });
+    });
+
+  }
+
+  getProductos( id: String ){
+    return this.http.get(`https://angular-html-49e53.firebaseio.com/productos/${id}.json`);
+  }
+
+
+  buscarProducto ( termino: string ){
+
+    if( this.productos.length === 0 ) {
+      // Se deben cargar los productos
+      this.cargarProductos().then(() => {
+        // Con ayuda del then ejecutamos el filtro, despues de haber cargado los productos
+        this.filtrarProductos( termino );
       });
 
+    } else {
+      // Aplicar el filtro
+      this.filtrarProductos( termino );
+    }   
+  }
+
+  private filtrarProductos ( termino: string ) {
+    // Pasamos a minusculas el termino para realizar la busqueda
+    termino = termino.toLowerCase();
+
+    // console.log(this.productos);
+    // Vaciamos el arreglo cada vez que el usuario realice una busqueda
+    this.productosFiltrado = [];
+    // Con el forEach recorremos y buscamos si en la categoria hay algun elemento que coincida con algo de la busqueda
+    this.productos.forEach( prod => {
+      // Variable para realizar la busqueda por titulo en minusculas 
+      const tituloLower = prod.titulo.toLowerCase();
+
+      if (  prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        // Si coincide lo agregamos al arreglo para despues mostrarlo en los resultados
+        this.productosFiltrado.push( prod );
+      }
+    });
   }
 }
